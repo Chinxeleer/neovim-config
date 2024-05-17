91940ef0f554633ddb7d5589d4c0e8ccec50ffb9
Blessing Kodze <blessingeleer.programming@gmail.com>
2024-05-17 15:52:50 +0200

    cleaned some files

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,48 +1,4 @@
 return {
-  -- {
-  --   "nvim-tree/nvim-tree.lua",
-  --   cmd = { "NvimTreeToggle", "NvimTreeFocus" },
-  --   config = function()
-  --     require("nvim-tree").setup({
-  --       disable_netrw = true,
-  --       hijack_netrw = true,
-  --       open_on_tab = false,
-  --       hijack_cursor = true,
-  --       hijack_unnamed_buffer_when_opening = false,
-  --       update_cwd = true,
-  --       update_focused_file = {
-  --         enable = true,
-  --         update_cwd = false,
-  --       },
-  --       sync_root_with_cwd = true,
-  --       view = {
-  --         width = 20,
-  --         hide_root_folder = false,
-  --       },
-  --       git = {
-  --         enable = false,
-  --         ignore = true,
-  --       },
-  --       actions = {
-  --         open_file = {
-  --           resize_window = true,
-  --         },
-  --       },
-  --       renderer = {
-  --         indent_markers = {
-  --           enable = false,
-  --         },
-  --       },
-  --     })
-  --   end,
-  -- },
-  -- {
-  --   "nvim-telescope/telescope.nvim",
-  --   cmd = "Telescope",
-  --   config = function()
-  --     require("telescope").setup({})
-  --   end,
-  -- },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
@@ -74,6 +30,34 @@ return {
       -- your config goes here
       -- or just leave it empty :)
     },
+  },
+  {
+    "b0o/incline.nvim",
+    priority = 1200,
+    -- event = "VeryLazy",
+    config = function()
+      local colors = require("catppuccin.palettes").get_palette("mocha")
+      require("incline").setup({
+        highlight = {
+          groups = {
+            InclineNormal = { guibg = colors.mantle, guifg = colors.mauve },
+            InclineNormalNC = { guibg = colors.mantle, guifg = colors.rosewater },
+          },
+        },
+        window = {
+          margin = { vertical = 0, horizontal = 1 },
+        },
+        hide = { cursorline = true },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          if vim.bo[props.buf].modified then
+            filename = "[+]" .. filename
+          end
+          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+          return { { icon, guifg = color }, { " " }, { filename } }
+        end,
+      })
+    end,
   },
   {
     "barrett-ruth/live-server.nvim",
